500e200ec2b5592930353c6b1924424e7ab64c79
John Shaffer <john@insilica.co>
2023-02-08 15:31:41 -0600

    Deploy to static.sysrev.com on push
    index.xhtml was renamed to index.html because the static website
    configuration looks for index.html

--- flake.nix
+++ flake.nix
@@ -1,29 +1,17 @@
 {
-  description = "srvc brat";
+  description = "srvc-brat";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    srvc.url = "github:insilica/rs-srvc";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, srvc, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; };
-      let
-        srvc-brat = stdenv.mkDerivation {
-          name = "srvc-brat";
-          src = ./.;
-
-          installPhase = ''
-            mkdir -p $out
-          '';
-        };
-      in {
-        packages = {
-          inherit srvc-brat;
-          default = srvc-brat;
-        };
-        devShells.default =
-          mkShell { buildInputs = [ srvc.packages.${system}.default ]; };
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell { buildInputs = [ awscli ]; };
       });
 }
